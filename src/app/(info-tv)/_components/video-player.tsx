9c2e2301cec6db95394a3c26785653ab3878a47b
evansmaina821 <evansmaina821@gmail.com>
2024-07-07 21:32:21 +0000

    added other trailers

--- src/app/(info-tv)/_components/video-player.tsx
+++ src/app/(info-tv)/_components/video-player.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+
 import { CldImage } from "next-cloudinary";
 
 import "@vidstack/react/player/styles/default/theme.css";
@@ -20,56 +20,30 @@ import {
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 
-
-interface Trailer {
-  id: string;
-  snippet: {
-    title: string;
-    thumbnails: {
-      high: {
-        url: string;
-      };
-    };
-  }
+interface TrailerInfo {
+  key: string;
+  name: string;
+  seasonNumber?: number;
 }
 
-interface Videoplayer {
+interface VideoPlayerProps {
   sources: string[];
-  info: string[];
+  info: TrailerInfo[];
 }
 
-interface TrailerData {
-  items: Trailer[];
-}
-
-interface TrailerImagesParams {
-  id: string;
-}
-
-
-async function TrailerImages({ id }: TrailerImagesParams): Promise<TrailerData> {
-  const apiKey = "AIzaSyDUDmLll7eVZYFxi0u8VbrdkRSYNWylcZA";
-  const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${apiKey}&part=snippet`,
-  );
-  const data = await res.json() as TrailerData;
-  return data;
-}
-
-export function VideoPlayer({ sources, info }: Videoplayer) {
-  const { data, isLoading, error } = useQuery<TrailerData>({
-    queryKey: ["trailer-images", info],
-    queryFn: () => TrailerImages({ id: info[0] ?? '' })
-  });
-
-  const [src, setSrc] = useState(0);
+export function VideoPlayer({ sources, info }: VideoPlayerProps) {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   function prevVideo() {
-    setSrc((n) => Math.max(0, n - 1));
+    setCurrentIndex((n) => Math.max(0, n - 1));
   }
 
   function nextVideo() {
-    setSrc((n) => Math.min(sources.length - 1, n + 1));
+    setCurrentIndex((n) => Math.min(sources.length - 1, n + 1));
+  }
+
+  function selectVideo(index: number) {
+    setCurrentIndex(index);
   }
 
   function onProviderChange(provider: MediaProviderAdapter | null) {
@@ -79,63 +53,71 @@ export function VideoPlayer({ sources, info }: Videoplayer) {
   }
 
   return (
-    <div className="flex w-full h-fit flex-col  lg:flex-row lg:justify-between gap-6">
-      <div className="w-full h-fit shadow-white-lg lg:max-w-[700px] xl:max-w-[1200px]">
-        <MediaPlayer
-          autoPlay={true}
-          src={sources[src]}
-          aspectRatio="16/9"
-          onProviderChange={onProviderChange}
-          playsInline
-          streamType="on-demand"
-          viewType="video"
-          logLevel="warn"
-          crossOrigin
-          onEnded={nextVideo}
-        >
-          <MediaProvider />
-          <DefaultVideoLayout icons={defaultLayoutIcons} />
-        </MediaPlayer>
+    <div className="flex h-fit w-full flex-col gap-6 lg:flex-row lg:justify-between">
+      <div className="h-fit w-full shadow-white-lg lg:max-w-[700px] xl:max-w-[1200px]">
+        {sources[currentIndex] && (
+          <MediaPlayer
+            autoPlay={true}
+            src={sources[currentIndex]}
+            aspectRatio="16/9"
+            onProviderChange={onProviderChange}
+            playsInline
+            streamType="on-demand"
+            viewType="video"
+            logLevel="warn"
+            crossOrigin
+            onEnded={nextVideo}
+          >
+            <MediaProvider />
+            <DefaultVideoLayout icons={defaultLayoutIcons} />
+          </MediaPlayer>
+        )}
       </div>
 
       <div>
-        <div className="flex flex-row justify-between mb-3">
-          <h1 className="font-medium text-xl">Related Trailers</h1>
+        <div className="mb-3 flex flex-row justify-between">
+          <h1 className="text-xl font-medium">Related Trailers</h1>
           <div className="flex flex-row gap-4">
-            <Button size={'sm'} onClick={prevVideo}>Prev Trailer</Button>
-            <Button size={'sm'} onClick={nextVideo}>Next Trailer</Button>
+            <Button size="sm" onClick={prevVideo}>
+              Prev Trailer
+            </Button>
+            <Button size="sm" onClick={nextVideo}>
+              Next Trailer
+            </Button>
           </div>
         </div>
 
-        <ScrollArea className="lg:max-h-[700px] lg:h-full h-[500px] lg:max-w-[500px] md:w-full w-full rounded-md border p-4">
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p>Error: {error.message}</p>
-          ) : (
-            <>
-              {data?.items.map((item: Trailer, index: number) => (
-                <div key={item.id || index} className="flex flex-col gap-4 ">
-                  <div className={` flex flex-row items-start gap-4 py-4 px-2 rounded-lg ${sources[src] === `youtube/${item.id}` ? 'bg-black/50' : ''}`}>
-                    {item.snippet?.thumbnails?.high?.url && (
-                      <CldImage
-                        src={item.snippet.thumbnails.high.url}
-                        alt={item.snippet.title}
-                        width={168}
-                        deliveryType="fetch"
-                        height={94}
-                        className="aspect-video rounded-lg object-cover"
-                      />
-                    )}
-                    <div className="grid gap-1 text-sm">
-                      <h2 className="font-medium">{item.snippet?.title}</h2>
-                      <h2>Trailer</h2>
-                    </div>
-                  </div>
+        <ScrollArea className="h-[500px] w-full rounded-md border p-4 md:w-full lg:h-full lg:max-h-[700px] lg:max-w-[500px]">
+          {info.map((item, index) => (
+            <div
+              key={item.key}
+              className="flex cursor-pointer flex-col gap-4"
+              onClick={() => selectVideo(index)}
+            >
+              <div
+                className={`flex flex-row items-start gap-4 rounded-lg px-2 py-4 ${index === currentIndex ? "bg-black/50" : ""}`}
+              >
+                <CldImage
+                  src={`https://img.youtube.com/vi/${item.key}/hqdefault.jpg`}
+                  alt={item.name}
+                  width={168}
+                  deliveryType="fetch"
+                  height={94}
+                  className="aspect-video rounded-lg object-cover"
+                />
+                <div className="grid gap-1 text-sm">
+                  <h2 className="font-medium">{item.name}</h2>
+                  {item.seasonNumber !== undefined && (
+                    <h2>
+                      {item.seasonNumber === 0
+                        ? "General Trailer"
+                        : `Season ${item.seasonNumber}`}
+                    </h2>
+                  )}
                 </div>
-              ))}
-            </>
-          )}
+              </div>
+            </div>
+          ))}
         </ScrollArea>
       </div>
     </div>
--- src/app/(info-tv)/info/[media_type]/[id]/page.tsx
+++ src/app/(info-tv)/info/[media_type]/[id]/page.tsx
@@ -1,5 +1,3 @@
-// /info/[media_type]/[id]/page.tsx
-
 import { VideoPlayer } from "@/app/(info-tv)/_components/video-player";
 import { Separator } from "@/components/ui/separator";
 import { Icons } from "@/components/ui/icons";
@@ -19,125 +17,6 @@ interface Movie {
   backdrop_path: string;
 }
 
-export async function generateMetadata({ params }: { params: { media_type: string, id: string } }) {
-  const { media_type, id } = params;
-  const res = await fetch(
-    `https://api.themoviedb.org/3/${media_type}/${id}?language=en-US`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
-      },
-    },
-  );
-  const data = await res.json() as Movie;
-
-  const {
-    name,
-    original_name,
-    overview,
-    title, 
-  } = data;
-
-  return {
-    title: name || original_name || title,
-    description: overview,
-  }
-}
-
-
-
-export default async function Page({ params: { media_type, id } }: { params: { media_type: string, id: string } }) {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/${media_type}/${id}?language=en-US`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
-      },
-    },
-  );
-  const data = await res.json() as Movie;
-
-
-
-  const {
-    name,
-    original_name,
-    overview,
-    vote_average,
-    title,
-    tagline,
-    backdrop_path,
-  } = data;
-
-  // Convert the number to a string
-  const vote_average_str = vote_average?.toString();
-
-  // Extract the first two characters from the string representation
-  const vote = vote_average_str.slice(0, 3);
-
-  return (
-    <div className="relative min-h-screen">
-      <ImageComponent
-        src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
-        alt={original_name}
-        fill={true}
-        
-        className="h-[550px] bg-cover bg-top object-cover lg:max-h-[800px] lg:h-full"
-      />
-      <div className="absolute bottom-0 left-0 right-0 top-0 h-[`calc(100vh - 550px)`] w-full bg-gradient-to-b from-transparent to-black " />
-      <div className="absolute bottom-0 left-0 right-0 top-0">
-        <div className="mx-auto lg:mt-0 mt-16 flex w-full max-w-[1770px] items-center rounded-lg px-3 py-0 lg:px-10 lg:py-3">
-          <div className=" flex w-full flex-col gap-10  lg:gap-10">
-            <div className=" ">
-              <TrailerInfo media_type={media_type} id={id} />
-            </div>
-
-            <div className="flex w-full max-w-[1200px]  flex-col gap-3 px-3  lg:gap-4 lg:px-0">
-
-              {/* Name */}
-              <h1 className="font-mono text-2xl lg:text-4xl">
-                {name || original_name || title}
-              </h1>
-
-              {/* Rating & Tagline */}
-              <div className="flex flex-row gap-4">
-                <p className="text-sm italic lg:text-lg">{tagline}</p> -
-                <div className="flex flex-row items-center gap-1">
-                  <Icons.star />
-                  <p className="text-sm lg:text-lg">{vote || null}</p>
-                </div>
-
-              </div>
-
-              <Separator />
-
-              {/* Overview */}
-              <p className="text-sm lg:text-lg">{overview}</p>
-
-              <Separator />
-
-              {/* Comment section */}
-              <div className="flex flex-col gap-8">
-                <h1 className="text-2xl font-medium ">Comments</h1>
-                <div>
-                  <Disqus id={id} title={name || original_name || title} media_type={media_type} />
-                </div>
-              </div>
-
-
-            </div>
-          </div>
-        </div>
-      </div>
-      <pre></pre>
-    </div>
-  );
-}
-
 interface Video {
   iso_639_1: string;
   iso_3166_1: string;
@@ -149,36 +28,217 @@ interface Video {
   official: boolean;
   published_at: string;
   id: string;
+  seasonNumber?: number;
 }
 
 interface ApiResponse {
   results: Video[];
 }
 
+interface Season {
+  season_number: number;
+}
+
+interface ShowData {
+  seasons: Season[];
+}
+
 interface TrailerInfoProps {
-  media_type: string;
+  media_type: "movie" | "tv";
   id: string;
 }
 
-const TrailerInfo: React.FC<TrailerInfoProps> = async ({ media_type, id }) => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/${media_type}/${id}/videos?language=en-US`,
+interface TrailerInfo {
+  key: string;
+  name: string;
+  seasonNumber?: number;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { media_type: string; id: string };
+}) {
+  const { media_type, id } = params;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${media_type}/${id}?language=en-US`,
     {
+      method: "GET",
       headers: {
+        "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
       },
     },
   );
-  const data = await response.json() as ApiResponse;
+  const data = (await res.json()) as Movie;
 
-  // Filter the results to include only those with type "Trailer"
-  const trailers = data.results.filter((video) => video.type === "Trailer");
+  const { name, original_name, overview, title } = data;
+
+  return {
+    title: name || original_name || title,
+    description: overview,
+  };
+}
+
+export default async function Page({
+  params: { media_type, id },
+}: {
+  params: { media_type: string; id: string };
+}) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${media_type}/${id}?language=en-US`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+      },
+    },
+  );
+  const data = (await res.json()) as Movie;
+
+  const {
+    name,
+    original_name,
+    overview,
+    vote_average,
+    title,
+    tagline,
+    backdrop_path,
+  } = data;
+
+  const vote_average_str = vote_average?.toString();
+  const vote = vote_average_str.slice(0, 3);
+
+  return (
+    <div className="relative min-h-screen">
+      <ImageComponent
+        src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+        alt={original_name}
+        fill={true}
+        className="h-[550px] bg-cover bg-top object-cover lg:h-full lg:max-h-[800px]"
+      />
+      <div className="h-[`calc(100vh - 550px)`] absolute bottom-0 left-0 right-0 top-0 w-full bg-gradient-to-b from-transparent to-black " />
+      <div className="absolute bottom-0 left-0 right-0 top-0">
+        <div className="mx-auto mt-16 flex w-full max-w-[1770px] items-center rounded-lg px-3 py-0 lg:mt-0 lg:px-10 lg:py-3">
+          <div className=" flex w-full flex-col gap-10  lg:gap-10">
+            <div className=" ">
+              <TrailerInfo media_type={media_type as "movie" | "tv"} id={id} />
+            </div>
+
+            <div className="flex w-full max-w-[1200px]  flex-col gap-3 px-3  lg:gap-4 lg:px-0">
+              <h1 className="font-mono text-2xl lg:text-4xl">
+                {name || original_name || title}
+              </h1>
+
+              <div className="flex flex-row gap-4">
+                <p className="text-sm italic lg:text-lg">{tagline}</p> -
+                <div className="flex flex-row items-center gap-1">
+                  <Icons.star />
+                  <p className="text-sm lg:text-lg">{vote || null}</p>
+                </div>
+              </div>
+
+              <Separator />
+
+              <p className="text-sm lg:text-lg">{overview}</p>
+
+              <Separator />
+
+              <div className="flex flex-col gap-8">
+                <h1 className="text-2xl font-medium ">Comments</h1>
+                <div>
+                  <Disqus
+                    id={id}
+                    title={name || original_name || title}
+                    media_type={media_type}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const TrailerInfo: React.FC<TrailerInfoProps> = async ({ media_type, id }) => {
+  let allTrailers: Video[] = [];
+
+  if (media_type === "movie") {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+        },
+      },
+    );
+    const data = (await response.json()) as ApiResponse;
+    allTrailers = data.results.filter((video) => video.type === "Trailer");
+  } else if (media_type === "tv") {
+    const showResponse = await fetch(
+      `https://api.themoviedb.org/3/tv/${id}?language=en-US`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+        },
+      },
+    );
+    const showData = (await showResponse.json()) as ShowData;
+    const seasons: Season[] = showData.seasons.filter(
+      (season) => season.season_number > 0,
+    );
+
+    for (const season of seasons) {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/tv/${id}/season/${season.season_number}/videos?language=en-US`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+          },
+        },
+      );
+      const data = (await response.json()) as ApiResponse;
+      const seasonTrailers = data.results.filter(
+        (video) => video.type === "Trailer",
+      );
+      seasonTrailers.forEach((trailer) => {
+        trailer.seasonNumber = season.season_number;
+      });
+      allTrailers.push(...seasonTrailers);
+    }
+
+    const showTrailersResponse = await fetch(
+      `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+        },
+      },
+    );
+    const showTrailersData = (await showTrailersResponse.json()) as ApiResponse;
+    const showTrailers = showTrailersData.results.filter(
+      (video) => video.type === "Trailer",
+    );
+    showTrailers.forEach((trailer) => {
+      trailer.seasonNumber = 0;
+    });
+    allTrailers.push(...showTrailers);
+  }
+
+  const trailerInfo: TrailerInfo[] = allTrailers.map((trailer) => ({
+    key: trailer.key,
+    name: trailer.name,
+    seasonNumber: trailer.seasonNumber,
+  }));
 
   return (
     <div className="flex flex-col gap-3 lg:gap-5">
       <VideoPlayer
-        sources={trailers.map((trailer) => `youtube/${trailer.key}`)}
-        info={trailers.map((trailer) => trailer.key)}
+        sources={allTrailers.map((trailer) => `youtube/${trailer.key}`)}
+        info={trailerInfo}
       />
     </div>
   );
