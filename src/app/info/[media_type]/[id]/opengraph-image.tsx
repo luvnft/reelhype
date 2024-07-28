d39bf0573872651be73820c27bae2b00ce306cae
evansmaina821 <evansmaina821@gmail.com>
2024-07-28 18:29:25 +0000

    hellos

--- src/app/info/[media_type]/[id]/opengraph-image.tsx
+++ src/app/info/[media_type]/[id]/opengraph-image.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from 'next/og';
 import ImageComponent from '@/components/image-component';
 
-export const runtime = 'edge';
 
 export const alt = 'Convofy Info';
 export const size = {
