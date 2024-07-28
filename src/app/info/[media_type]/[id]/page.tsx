5311f4cdef074549e6a4bdcf015b965a77a636af
evansmaina821 <evansmaina821@gmail.com>
2024-07-28 18:41:51 +0000

    fixed bug

--- src/app/info/[media_type]/[id]/page.tsx
+++ src/app/info/[media_type]/[id]/page.tsx
@@ -3,6 +3,21 @@ import { FilmInfo } from '@/server/tmdb';
 import { Disqus } from '../../_components/disqus';
 import { TrailerInfo } from '../../_components/trailer-info';
 
+
+interface Backdrop {
+    aspect_ratio: number;
+    height: number;
+    iso_639_1: string;
+    file_path: string;
+    vote_average: number;
+    vote_count: number;
+    width: number;
+  }
+  
+  interface BackdropProps {
+    backdrops: Backdrop[];
+  }
+
 export async function generateMetadata({
     params,
 }: {
@@ -10,12 +25,41 @@ export async function generateMetadata({
 }) {
     const { media_type, id } = params;
     const data = await FilmInfo({ media_type: media_type, id: id });
+    const post = await fetch(
+        `https://api.themoviedb.org/3/${params.media_type}/${params.id}/images?include_image_language=en&language=en-US`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
+          },
+          cache: 'no-store',
+        }
+      ).then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json() as Promise<BackdropProps>;
+      }).catch((error) => {
+        console.error('Error fetching backdrop:', error);
+        return { backdrops: [] } as BackdropProps;
+      });
+    
+      const backdropPath = post?.backdrops?.[0]?.file_path ?? '/fallback-image.jpg';
 
     const { name, original_name, overview, title } = data;
 
     return {
         title: `${name || original_name || title} Trailer`,
         description: overview,
+        openGraph: {
+            images: {
+                url: `https://image.tmdb.org/t/p/original${backdropPath}`,
+                width: 1200,
+                height: 630,
+                alt: `${name || original_name || title} Trailer`,
+            }
+        }
     };
 }
 
