9b5429ad037fac433f1206f027b51f922db6a617
evansmaina821 <evansmaina821@gmail.com>
2024-07-31 10:44:18 +0000

    added sitemaps

---
+++ src/app/Search/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Convofy',
+        short_name: 'Convofy',
+        description: 'Ultimate Destination for Movie & TV Trailers',
+        start_url: '/Search',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#000000',
+        icons: [
+            {
+                src: '/favicon.ico',
+                sizes: 'any',
+                type: 'image/x-icon',
+            },
+        ],
+    };
+}
