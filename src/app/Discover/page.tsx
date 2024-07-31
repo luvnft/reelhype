3eca94556bcc25895a7d15ea398b775dcd28edcb
evansmaina821 <evansmaina821@gmail.com>
2024-07-31 00:16:16 +0000

    fixed metadata

--- src/app/Discover/page.tsx
+++ src/app/Discover/page.tsx
@@ -4,10 +4,56 @@ import { Icons } from '@/components/ui/icons';
 import { TrendingFilms } from '@/server/tmdb';
 import type { Metadata } from 'next';
 import Link from 'next/link';
+import { siteConfig } from '@/config/site';
 
 export const metadata: Metadata = {
     title: 'Discover',
     description: 'Discover movies and tv trailers',
+    metadataBase: new URL(siteConfig.url),
+    keywords: [
+        'Movies',
+        'Tv Shows',
+        'Trailers',
+        'Collaboration',
+        'Movie Trailers',
+        'Tv Show Trailers',
+        "Discover"
+    ],
+    authors: [
+        {
+            name: 'Convofy',
+            url: 'https://reelhype.space',
+        },
+    ],
+    creator: 'Convofy',
+    openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: siteConfig.url,
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name,
+        images: [
+            {
+                url: siteConfig.ogImage,
+                width: 1200,
+                height: 630,
+                alt: siteConfig.name,
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: siteConfig.name,
+        description: siteConfig.description,
+        images: [siteConfig.ogImage],
+        creator: '@Convofy',
+    },
+    icons: {
+        icon: '/favicon.ico',
+        apple: '/apple-icon.png',
+    },
+    manifest: `${siteConfig.url}/sitemap.xml`,
 };
 
 export default async function Trending() {
--- src/app/Search/layout.tsx
+++ src/app/Search/layout.tsx
@@ -1,8 +1,53 @@
 import type { Metadata } from 'next';
+import { siteConfig } from '@/config/site';
 
 export const metadata: Metadata = {
     title: 'Search',
     description: 'Search for movies and tv trailers',
+    keywords: [
+        'Movies',
+        'Tv Shows',
+        'Trailers',
+        'Collaboration',
+        'Movie Trailers',
+        'Tv Show Trailers',
+        "Search"
+    ],
+    authors: [
+        {
+            name: 'Convofy',
+            url: 'https://reelhype.space/Search',
+        },
+    ],
+    creator: 'Convofy',
+    openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: siteConfig.url,
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name,
+        images: [
+            {
+                url: siteConfig.ogImage,
+                width: 1200,
+                height: 630,
+                alt: siteConfig.name,
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: siteConfig.name,
+        description: siteConfig.description,
+        images: [siteConfig.ogImage],
+        creator: '@Convofy',
+    },
+    icons: {
+        icon: '/favicon.ico',
+        apple: '/apple-icon.png',
+    },
+    manifest: `${siteConfig.url}/sitemap.xml`,
 };
 
 export default function SearchLayout({
