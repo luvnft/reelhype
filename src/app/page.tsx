3929354432b29560f47b545bc527ee43a4c81573
evansmaina821 <evansmaina821@gmail.com>
2024-08-02 19:06:37 +0000

    fixed some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,45 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import { ArrowUpRight } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
-import Image from "next/image"
 
 export default async function HomePage() {
-   
-
     return (
-        <main className="flex max-w-[1300px] justify-center lg:mt-32 md:mt-[71px] mt-20 mx-auto container min-h-screen">
-            <div className='flex flex-col text-center gap-6'>
-                <h1 className="font-secondary lg:text-[90.4px] md:text-[59px] text-[30px] ">
-                Lights, Camera, Preview!<br />Your Movie & TV Trailer Hub
+        <main className="container mx-auto mt-20 flex min-h-screen max-w-[1300px] justify-center md:mt-[71px] lg:mt-32">
+            <div className="flex flex-col gap-6 text-center">
+                <h1 className="font-secondary text-[30px] md:text-[59px] lg:text-[90.4px] ">
+                    Lights, Camera, Preview!
+                    <br />
+                    Your Movie & TV Trailer Hub
                 </h1>
 
-                <p className='lg:text-lg text-[16px] text-[#9C9C9C]'>
-                Watch the Latest Trailers, Join Live Premieres, and Connect with Fellow Fans.
+                <p className="text-[16px] text-[#9C9C9C] lg:text-lg">
+                    Watch the Latest Trailers, Join Live Premieres, and Connect
+                    with Fellow Fans.
                 </p>
 
-                <Button className='w-fit mx-auto flex flex-row gap-1' asChild  size="lg">
-                    <Link href={'/Discover'}>Watch Trending Trailers</Link>
-                    <ArrowUpRight />
-                </Button>
+                <Link href="/Discover">
+                    <Button
+                        className="mx-auto flex w-fit flex-row gap-1"
+                        size="lg"
+                    >
+                        Watch Trending Trailers
+                        <ArrowUpRight />
+                    </Button>
+                </Link>
 
                 <div className="flex flex-row gap-2">
-                   <h3 className='lg:text-lg text-[16px] text-[#9C9C9C]'>
-                    Powered by TMDB
-                </h3> 
-                <Image src="/tmdb-logo.svg" alt="tmdb logo" width={200} height={200}/>
+                    <h3 className="text-[16px] text-[#9C9C9C] lg:text-lg">
+                        Powered by
+                    </h3>
+                    <Image
+                        src="/tmdb-logo.svg"
+                        alt="tmdb logo"
+                        width={200}
+                        height={200}
+                    />
                 </div>
-                
             </div>
-
-           
         </main>
     );
 }
